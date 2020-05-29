e89fbdcaf7b57014c397d50ab8e5533c0a69092b
nasi7 <55255943+nasi7@users.noreply.github.com>
2020-05-29 14:49:14 -0500

    Added Database tables

---
+++ JooleDatabase/TVs.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TVs]
+(
+	[ModelNumber] VARCHAR(64) NOT NULL PRIMARY KEY,
+	[Power] VARCHAR(8),
+	[Voltage] VARCHAR(8),
+	[ScreenSize] VARCHAR(8),
+	[Dimension] VARCHAR(32),
+	[Resolution] VARCHAR(8),
+	[InputTypes] VARCHAR(32),
+	[OutputTypes] VARCHAR(32)
+)
---
+++ JooleDatabase/Toasters.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Toasters]
+(
+	[ModelNumber] VARCHAR(64) NOT NULL PRIMARY KEY,
+	[Power] VARCHAR(8),
+	[Voltage] VARCHAR(8),
+	[HeatingLevel] VARCHAR(8),
+	[Dimensions] VARCHAR(32)
+)
