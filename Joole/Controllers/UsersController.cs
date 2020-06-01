1c72490ad9ccb73a3fc1e11480eeb8e484191666
nasi7 <55255943+nasi7@users.noreply.github.com>
2020-06-01 12:23:01 -0500

    Implemented Onion Architecture with Entity Framework

---
+++ Joole/Controllers/UsersController.cs
@@ -0,0 +1,116 @@
+﻿using JooleCore;
+using JooleRepo;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Joole.Controllers
+{
+    public class UsersController : Controller
+    {
+        //private JooleDatabaseEntities db = new JooleDatabaseEntities();
+        private UnitOfWork unitOfWork = new UnitOfWork();
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = unitOfWork.UserRepository.GetAll();
+            return View(users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(string id)
+        {
+            User user = unitOfWork.UserRepository.Get(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        /*public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Username,Email,Password,Image")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Username,Email,Password,Image")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }*/
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ JooleRepo/UnitOfWork.cs
@@ -0,0 +1,64 @@
+﻿using JooleCore;
+using System;
+
+namespace JooleRepo
+{
+    public class UnitOfWork : IDisposable
+    {
+        // private readonly JooleDatabaseEntities _context;
+
+
+        private JooleDatabaseEntities context = new JooleDatabaseEntities();
+        private Repository<User> userRepository;
+
+
+        /* public UnitOfWork(PlutoContext context)
+         {
+             _context = context;
+             Courses = new CourseRepository(_context);
+             Authors = new AuthorRepository(_context);
+         }
+
+         public ICourseRepository Courses { get; private set; }
+         public IAuthorRepository Authors { get; private set; }*/
+
+        public Repository<User> UserRepository
+        {
+            get
+            {
+
+                if (this.userRepository == null)
+                {
+                    this.userRepository = new Repository<User>(context);
+                }
+                return userRepository;
+            }
+        }
+
+
+        public void Complete()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
