6496065eeb099a1c1be083f876358b128ba2c5ed
Maohan Sun <lampoon2gn@gmail.com>
2020-06-01 12:46:23 -0600

    update

--- JooleDatabase/TableSaws.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[TableSaws]
-(
-	[ModelNumber] VARCHAR(64) NOT NULL PRIMARY KEY,
-	[Power] VARCHAR(8),
-	[Voltage] VARCHAR(8),
-	[Amperes] VARCHAR(8),
-	[Dimension] VARCHAR(32),
-	[BladeHeight] VARCHAR(8)
-)
